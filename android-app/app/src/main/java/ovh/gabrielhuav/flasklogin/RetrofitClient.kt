d5e0e7620d99e0ee73d4cc783214cc639638efbb
JulioCesarCaballero <juliocesar64914@gmail.com>
2026-09-09 13:42:40 -0600

    Agregación de la IP

--- android-app/app/src/main/java/ovh/gabrielhuav/flasklogin/RetrofitClient.kt
+++ android-app/app/src/main/java/ovh/gabrielhuav/flasklogin/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // 10.0.2.2 apunta a tu PC desde el emulador (Docker corre ahí en el puerto 5000)
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    private const val BASE_URL = "http://10.203.147.187:5000/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
